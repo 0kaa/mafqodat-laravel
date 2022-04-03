eabdcaace2d31316c7ed8274087914523eea83f8
Kamal-Abouzayed <kamal.abouzayed@gmail.com>
2022-04-03 11:53:46 +0200

    📦 NEW: Dashboard Statistics

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
+use App\Models\Station;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('dashboard.home');
+
+        $items = Item::get()->count();
+
+        $delivered_items = Item::where('is_delivered', '1')->get()->count();
+
+        $stations = Station::get()->count();
+
+        $employees = User::whereDoesntHave('roles')->get()->count();
+
+        return view('dashboard.home', compact('items', 'delivered_items', 'stations', 'employees'));
     }
 }
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,50 +1,114 @@
 @extends('dashboard.layouts.app')
 
-@section('title' , __('control_panel'))
+@section('title', __('control_panel'))
 
 @section('content')
 
-<div class="app-content content ">
-    <div class="content-overlay"></div>
-    <div class="header-navbar-shadow"></div>
-    <div class="content-wrapper">
-        <div class="content-header row">
-        </div>
-        <div class="content-body">
-            <!-- Dashboard Analytics Start -->
-            <section id="dashboard-analytics">
-                <div class="row match-height">
-                    <!-- Greetings Card starts -->
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <div class="card card-congratulations">
-                            <div class="card-body text-center">
-                                <img src="{{ url('dashboard') }}/app-assets/images/elements/decore-left.png" class="congratulations-img-left" alt="card-img-left" />
-                                <img src="{{ url('dashboard') }}/app-assets/images/elements/decore-right.png" class="congratulations-img-right" alt="card-img-right" />
-                                <div class="avatar avatar-xl bg-primary shadow">
-                                    <div class="avatar-content">
-                                        <i data-feather="award" class="font-large-1"></i>
+    <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <!-- Dashboard Analytics Start -->
+                <section id="dashboard-analytics">
+                    <div class="row match-height">
+                        <!-- Greetings Card starts -->
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="card card-congratulations">
+                                <div class="card-body text-center">
+                                    <img src="{{ url('dashboard') }}/app-assets/images/elements/decore-left.png"
+                                        class="congratulations-img-left" alt="card-img-left" />
+                                    <img src="{{ url('dashboard') }}/app-assets/images/elements/decore-right.png"
+                                        class="congratulations-img-right" alt="card-img-right" />
+                                    <div class="avatar avatar-xl bg-primary shadow">
+                                        <div class="avatar-content">
+                                            <i data-feather="award" class="font-large-1"></i>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="text-center">
-                                    <h1 class="mb-1 text-white">{{ __('congrats') }} </h1>
+                                    <div class="text-center">
+                                        <h1 class="mb-1 text-white">{{ __('congrats') }} </h1>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- Greetings Card ends -->
+
+
+                        <!-- Statistics Card -->
+                        <div class="col-xl-12 col-md-12 col-12">
+                            <div class="card card-statistics">
+                                <div class="card-header">
+                                    <h4 class="card-title">{{ __('statistics') }}</h4>
+                                </div>
+                                <div class="card-body statistics-body">
+                                    <div class="row">
+                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                            <div class="media">
+                                                <div class="avatar bg-light-primary mr-2">
+                                                    <div class="avatar-content">
+                                                        <i data-feather="box" class="avatar-icon"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="media-body my-auto">
+                                                    <h4 class="font-weight-bolder mb-0 item-count">{{ $items }}</h4>
+                                                    <p class="card-text font-small-3 mb-0">{{ __('total_items') }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                            <div class="media">
+                                                <div class="avatar bg-light-info mr-2">
+                                                    <div class="avatar-content">
+                                                        <i data-feather="box" class="avatar-icon"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="media-body my-auto">
+                                                    <h4 class="font-weight-bolder mb-0">{{ $delivered_items }}</h4>
+                                                    <p class="card-text font-small-3 mb-0">
+                                                        {{ __('total_delivered_items') }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+                                            <div class="media">
+                                                <div class="avatar bg-light-danger mr-2">
+                                                    <div class="avatar-content">
+                                                        <i data-feather="compass" class="avatar-icon"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="media-body my-auto">
+                                                    <h4 class="font-weight-bolder mb-0">{{ $stations }}</h4>
+                                                    <p class="card-text font-small-3 mb-0">{{ __('total_stations') }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-sm-6 col-12">
+                                            <div class="media">
+                                                <div class="avatar bg-light-success mr-2">
+                                                    <div class="avatar-content">
+                                                        <i data-feather="users" class="avatar-icon"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="media-body my-auto">
+                                                    <h4 class="font-weight-bolder mb-0">{{ $employees }}</h4>
+                                                    <p class="card-text font-small-3 mb-0">{{ __('total_employees') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/ Statistics Card -->
                     </div>
-                    <!-- Greetings Card ends -->
+                </section>
+                <!-- Dashboard Analytics end -->
 
-
-
-            </section>
-            <!-- Dashboard Analytics end -->
-
+            </div>
         </div>
     </div>
-</div>
 
-
-
-@push('js')
-
-@endpush
 @endsection
