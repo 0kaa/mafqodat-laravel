5c4a6b4f7b6f8c1b7cb0a5061b9e102bb86dac0d
0kaa <thebestoka0@gmail.com>
2022-04-05 10:46:19 +0200

    👌 IMPROVE: Api categories

--- database/migrations/2022_03_15_074028_create_categories_table.php
+++ database/migrations/2022_03_15_074028_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name_ar');
             $table->string('name_en');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
