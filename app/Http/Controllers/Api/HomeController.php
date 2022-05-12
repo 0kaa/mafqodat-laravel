b50e9382a4f48d86c82e41d7681b5f176377049f
0kaa <thebestoka0@gmail.com>
2022-05-12 16:39:19 +0200

    👌 IMPROVE: Home Controller

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -15,24 +15,14 @@ class HomeController extends Controller
 
     public function latestItems()
     {
-        $latest_items = Item::latest()->take(5)->get();
-
-        if ($latest_items->isNotEmpty()) {
-            return $this->apiResponse('', ItemResource::collection($latest_items), 200);
-        } else {
-            return $this->apiResponse('', [], 404);
-        }
+        $latest_items = Item::latest()->take(3)->get();
+        return $this->apiResponse('', ItemResource::collection($latest_items), 200);
     }
 
     public function locations()
     {
         $locations = Station::select('lat', 'lng')->get();
-
-        if ($locations->isNotEmpty()) {
-            return $this->apiResponse('', $locations, 200);
-        } else {
-            return $this->apiResponse('', [], 404);
-        }
+        return $this->apiResponse('', $locations, 200);
     }
 
 
