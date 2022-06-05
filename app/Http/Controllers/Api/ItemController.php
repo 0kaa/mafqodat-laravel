c87edf78269cf430d018c52bb70700e487db1cae
KairoCom <kairo.com20@gmail.com>
2022-06-05 11:38:32 +0200

    👌 IMPROVE: item api

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -8,10 +8,12 @@ use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ItemResource;
 use App\Http\Resources\PaginationResource;
 use App\Http\Resources\StationResource;
+use App\Http\Resources\StorageResource;
 use App\Models\Category;
 use App\Models\Item;
 use App\Models\Log;
 use App\Models\Station;
+use App\Models\Storage as AppStorage;
 use App\Traits\ApiResponse;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -50,12 +52,38 @@ class ItemController extends Controller
 
         $data = $request->all();
 
-        if ($request->hasFile('image')) {
-            $data['image'] = $request->file('image')->store('items');
+        if($request->report_type == 'found') {
+            $data['informer_name'] = $request->informer_name;
+            $data['informer_phone'] = $request->informer_phone;
+        } else {
+            $data['informer_name'] = null;
+            $data['informer_phone'] = null;
         }
+
+        $data['report_number'] = random_int(00000, 99999);
+
         $data['user_id'] = $user->id;
 
         $item = Item::create($data);
+
+        if ($request->has('images')) {
+
+            $files = $request->file('images');
+
+            foreach ($files as $file) {
+
+                $image = $file->store('items');
+
+                $item->media()->create([
+
+                    'item_id' => $item->id,
+                    'image' => $image
+
+                ]); // end of create
+
+            } // end of foreach
+
+        } // end of has images
 
         Log::create([
             'user_id' => $user->id,
@@ -81,18 +109,19 @@ class ItemController extends Controller
 
             $validator = Validator::make($data, [
                 /* items data */
-                'details'          => 'required',
-                'category_id'      => 'required',
-                'station_id'       => 'required',
-                'storage'          => 'required',
-                'date'             => 'required',
-                'time'             => 'required',
-                'primary_colour'   => 'required',
-                'secondary_colour' => 'sometimes',
-                'tertiary_colour'  => 'sometimes',
-                'description'      => 'sometimes',
-                'image'            => 'sometimes',
-                'is_delivered'     => 'sometimes',
+                'report_type'    => 'required',
+                'informer_name'  => 'required_if:report_type,found',
+                'informer_phone' => 'required_if:report_type,found',
+                'details'        => 'required',
+                'category_id'    => 'required',
+                'station_id'     => 'required',
+                'storage_id'     => 'required',
+                'date'           => 'required',
+                'time'           => 'required',
+                'is_delivered'   => 'sometimes',
+                'report_type'    => 'required',
+                'informer_name'  => 'required_if:report_type,found',
+                'informer_phone' => 'required_if:report_type,found',
 
                 /* User data */
                 'first_name'        => 'nullable',
@@ -105,30 +134,32 @@ class ItemController extends Controller
                 'mobile'            => 'nullable',
                 'email'             => 'nullable',
             ], [
-                'details.required'        => __('details_required'),
-                'category_id.required'    => __('category_id_required'),
-                'station_id.required'     => __('station_id_required'),
-                'storage.required'        => __('storage_required'),
-                'date.required'           => __('date_required'),
-                'time.required'           => __('time_required'),
-                'primary_colour.required' => __('primary_colour_required'),
-                'first_name.required_if'  => __('first_name_required'),
-                'first_name.min'          => __('first_name_min'),
-                'surname.required_if'     => __('family_name_required'),
-                'surname.min'             => __('family_name_min'),
-                'email.required_if'       => __('email_required'),
-                'email.email'             => __('must_be_email'),
-                'address.required_if'     => __('address_required'),
-                'address.min'             => __('address_min'),
-                'secondary_address.min'   => __('secondary_address_min'),
-                'postcode.required_if'    => __('postcode_required'),
-                'city.required_if'        => __('city_required'),
-                'phone.required_if'       => __('phone_required'),
-                'phone.min'               => __('phone_min'),
-                'phone.max'               => __('phone_max'),
-                'mobile.required_if'      => __('mobile_required'),
-                'mobile.min'              => __('mobile_min'),
-                'mobile.max'              => __('mobile_max'),
+                'details.required'           => __('details_required'),
+                'category_id.required'       => __('category_id_required'),
+                'station_id.required'        => __('station_id_required'),
+                'storage_id.required'        => __('storage_required'),
+                'date.required'              => __('date_required'),
+                'time.required'              => __('time_required'),
+                'report_type.required'       => __('report_type_required'),
+                'informer_name.required_if'  => __('informer_name_required'),
+                'informer_phone.required_if' => __('informer_phone_required'),
+                'first_name.required_if'     => __('first_name_required'),
+                'first_name.min'             => __('first_name_min'),
+                'surname.required_if'        => __('family_name_required'),
+                'surname.min'                => __('family_name_min'),
+                'email.required_if'          => __('email_required'),
+                'email.email'                => __('must_be_email'),
+                'address.required_if'        => __('address_required'),
+                'address.min'                => __('address_min'),
+                'secondary_address.min'      => __('secondary_address_min'),
+                'postcode.required_if'       => __('postcode_required'),
+                'city.required_if'           => __('city_required'),
+                'phone.required_if'          => __('phone_required'),
+                'phone.min'                  => __('phone_min'),
+                'phone.max'                  => __('phone_max'),
+                'mobile.required_if'         => __('mobile_required'),
+                'mobile.min'                 => __('mobile_min'),
+                'mobile.max'                 => __('mobile_max'),
             ]);
 
             if ($validator->stopOnFirstFailure()->fails()) {
@@ -159,18 +190,37 @@ class ItemController extends Controller
                 $data['mobile'] = null;
             }
 
-            if ($request->hasFile('image')) {
+            if ($request->has('images')) {
 
-                if ($item->image !== null) {
-                    if (Storage::exists($item->image)) {
-                        Storage::delete($item->image);
-                    }
-                }
+                $files = $request->file('images');
 
-                $data['image'] = $request->file('image')->store('items');
+                foreach ($files as $file) {
+
+                    $image = $file->store('items');
+
+                    $item->media()->create([
+
+                        'item_id' => $item->id,
+                        'image' => $image
+
+                    ]); // end of create
+
+                } // end of foreach
+
+            } // end of has images
+
+            if ($request->report_type == 'found') {
+
+                $data['informer_name'] = $request->informer_name;
+                $data['informer_phone'] = $request->informer_phone;
+
             } else {
-                $data['image'] = $item->image;
+
+                $data['informer_name'] = null;
+                $data['informer_phone'] = null;
+
             }
+
 
             $item->update($data);
 
@@ -241,5 +291,12 @@ class ItemController extends Controller
         $stations = Station::all();
 
         return $this->apiResponse('', StationResource::collection($stations), 200);
+    }
+
+    public function storageList()
+    {
+        $storages = AppStorage::all();
+
+        return $this->apiResponse('', StorageResource::collection($storages), 200);
     }
 }
