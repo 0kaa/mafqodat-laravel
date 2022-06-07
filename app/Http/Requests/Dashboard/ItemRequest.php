41dee87891ab8935b44117e969f57223f269e3bb
KairoCom <kairo.com20@gmail.com>
2022-06-07 11:25:16 +0200

    👌 IMPROVE: item request

--- app/Http/Requests/Dashboard/ItemRequest.php
+++ app/Http/Requests/Dashboard/ItemRequest.php
@@ -29,9 +29,22 @@ class ItemRequest extends FormRequest
             'station_id'  => 'required',
             'storage_id'  => 'required',
             'date'        => 'required',
+            'time'        => 'required',
             'description' => 'sometimes',
             'image'       => 'sometimes',
             'report_type' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'category_id.required'       => __('category_id_required'),
+            'station_id.required'        => __('station_id_required'),
+            'storage_id.required'        => __('storage_required'),
+            'date.required'              => __('date_required'),
+            'time.required'              => __('time_required'),
+            'report_type.required'       => __('report_type_required'),
         ];
     }
 }
