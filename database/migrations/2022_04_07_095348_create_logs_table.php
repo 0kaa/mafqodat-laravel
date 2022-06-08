4a8b66b0bf01ba2b2a5dcfeb4d458a5f2a01da80
Kamal-Abouzayed <kamal.abouzayed@gmail.com>
2022-06-08 15:36:47 +0200

    👌 IMPROVE: logs

--- database/migrations/2022_04_07_095348_create_logs_table.php
+++ database/migrations/2022_04_07_095348_create_logs_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('item_id')->nullable();
-            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
             $table->string('message_ar');
             $table->string('message_en');
             $table->string('date');
