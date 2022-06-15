035cd39bba7eda1c01fac3cda3cfdc0168457708
Kamal-Abouzayed <Kamal.abouzayed@gmail.com>
2022-06-15 12:34:54 +0200

    🐛 FIX: sidebar scroll

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,79 +1,139 @@
 <!-- BEGIN: Main Menu-->
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
 
-    <div class="img_menu_01">
-        <img src="{{ asset('dashboard/app-assets/images/pages/station_logo.jpg') }}" alt="logo">
+
+
+
+
+
+
+    <div class="navbar-header">
+        <div class="img_menu_01">
+            <img src="{{ asset('dashboard/app-assets/images/pages/station_logo.jpg') }}" alt="logo">
+        </div>
     </div>
+
+
+
+
+
     <div class="shadow-bottom"></div>
-    <div class="main-menu-content ps ps__rtl ps--active-y" >
+    <div class="main-menu-content">
+
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class="nav-item {{ isActiveRoute('admin.home') }}"><a class="d-flex align-items-center" href="{{ route('admin.home') }}"><i data-feather="home"></i><span class="menu-title text-truncate">{{ __('home') }}</span></a>
+            <li class="nav-item {{ isActiveRoute('admin.home') }}"><a class="d-flex align-items-center"
+                    href="{{ route('admin.home') }}"><i data-feather="home"></i><span
+                        class="menu-title text-truncate">{{ __('home') }}</span></a>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.employees.create' , 'admin.employees.index' , 'admin.employees.edit', 'admin.get_logs']) }}"><a class="d-flex align-items-center" href="#"><i data-feather="users"></i><span class="menu-title text-truncate">{{ __('employees') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.employees.create', 'admin.employees.index', 'admin.employees.edit', 'admin.get_logs']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather="users"></i><span
+                        class="menu-title text-truncate">{{ __('employees') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.employees.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('employees_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.employees.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('employees_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.employees.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_employee') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.employees.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_employee') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.get_logs') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('employee_logs') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.get_logs') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('employee_logs') }}</span></a>
                     </li>
                 </ul>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.cities.create' , 'admin.cities.index' , 'admin.cities.edit']) }}"><a class="d-flex align-items-center" href="#"><i data-feather='map-pin'></i><span class="menu-title text-truncate">{{ __('cities') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.cities.create', 'admin.cities.index', 'admin.cities.edit']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather='map-pin'></i><span
+                        class="menu-title text-truncate">{{ __('cities') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.cities.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('cities_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.cities.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('cities_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.cities.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_city') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.cities.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_city') }}</span></a>
                     </li>
                 </ul>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.stations.create' , 'admin.stations.index' , 'admin.stations.edit']) }}"><a class="d-flex align-items-center" href="#"><i data-feather='compass'></i><span class="menu-title text-truncate">{{ __('stations') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.stations.create', 'admin.stations.index', 'admin.stations.edit']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather='compass'></i><span
+                        class="menu-title text-truncate">{{ __('stations') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.stations.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('stations_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.stations.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('stations_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.stations.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_station') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.stations.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_station') }}</span></a>
                     </li>
                 </ul>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.categories.create' , 'admin.categories.index' , 'admin.categories.edit']) }}"><a class="d-flex align-items-center" href="#"><i data-feather='inbox'></i><span class="menu-title text-truncate">{{ __('categories') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.categories.create', 'admin.categories.index', 'admin.categories.edit']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather='inbox'></i><span
+                        class="menu-title text-truncate">{{ __('categories') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.categories.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('categories_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.categories.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('categories_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.categories.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_category') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.categories.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_category') }}</span></a>
                     </li>
                 </ul>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.storages.create' , 'admin.storages.index' , 'admin.storages.edit']) }}"><a class="d-flex align-items-center" href="#"><i data-feather='inbox'></i><span class="menu-title text-truncate">{{ __('storages') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.storages.create', 'admin.storages.index', 'admin.storages.edit']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather='inbox'></i><span
+                        class="menu-title text-truncate">{{ __('storages') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.storages.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('storages_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.storages.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('storages_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.storages.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_storage') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.storages.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_storage') }}</span></a>
                     </li>
                 </ul>
             </li>
         </ul>
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li  class="nav-item {{ areActiveRoutes(['admin.items.create' , 'admin.items.index' , 'admin.items.edit']) }}"><a class="d-flex align-items-center" href="#"><i data-feather='package'></i><span class="menu-title text-truncate">{{ __('items') }}</span></a>
+            <li
+                class="nav-item {{ areActiveRoutes(['admin.items.create', 'admin.items.index', 'admin.items.edit']) }}">
+                <a class="d-flex align-items-center" href="#"><i data-feather='package'></i><span
+                        class="menu-title text-truncate">{{ __('items') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.items.index') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('items_list') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.items.index') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('items_list') }}</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="{{ route('admin.items.create') }}"><i data-feather="circle"></i><span class="menu-item text-truncate" >{{ __('new_item') }}</span></a>
+                    <li><a class="d-flex align-items-center" href="{{ route('admin.items.create') }}"><i
+                                data-feather="circle"></i><span
+                                class="menu-item text-truncate">{{ __('new_item') }}</span></a>
                     </li>
                 </ul>
             </li>
